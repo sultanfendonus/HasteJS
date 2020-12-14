91239c1f793093cf14067ff69891963cc39e0a4f
Sunny Sultan <sultan1640@gmail.com>
2020-12-14 20:51:53 +0600

    removed unnessary files

--- haste/module/controller/mapper.js
+++ haste/module/controller/mapper.js
@@ -1,11 +1,7 @@
 import user from "../../../app/user/controller.js";
-import category from "../../../app/category/controller.js";
-import post from "../../../app/post/controller.js";
 //IMPORT
 
 export const CONTROLLER_MAPPER = {
     user: user,
-    category: category,
-    post: post,
     //CONTROLLERS
 }
