6b5c8095b887d32d6d1d50860b151e971c6aaab1
Sunny Sultan <sultan1640@gmail.com>
2020-12-15 14:43:57 +0600

    file generator redesign

--- app/controllerMapper.js
+++ app/controllerMapper.js
@@ -1,11 +1,8 @@
 import user from "./user/controller.js";
-import test2 from "./test2/controller.js";
 import test3 from "./test3/controller.js";
 //IMPORT
 
 export const CONTROLLER_MAPPER = {
     user: user,
-    test2: test2,
-    test3: test3,
-    //CONTROLLERS
+    test3: test3
 }
